e5a840898bca342c59cf8f0914641a816b32838f
Zeitcatcher <ars.oksenoyt@gmail.com>
2023-11-14 17:55:57 -0800

    Created main screen

---
+++ GameWorld/ViewControllers/PlatformCollectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PlatformCollectionViewController.swift
+//  GameWorld
+//
+//  Created by Arseniy Oksenoyt on 11/4/23.
+//
+
+import UIKit
+
+class PlatformCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
