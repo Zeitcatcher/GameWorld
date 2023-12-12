4c61792dae6012ce6ec4ba0a600df8a87859fa9c
Zeitcatcher <ars.oksenoyt@gmail.com>
2023-12-11 19:17:40 -0800

    Created GameDetailsViewController file

---
+++ GameWorld/ViewControllers/GameDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GameDetailsViewController.swift
+//  GameWorld
+//
+//  Created by Arseniy Oksenoyt on 12/11/23.
+//
+
+import UIKit
+
+class GameDetailsViewController: UIViewController {
+    
+    private var screenshotsImageView: UIImageView!
+    private var backgroundView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
