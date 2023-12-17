b44bf39a63f2aaa45efac71a4ead9fce8d7299cc
Zeitcatcher <ars.oksenoyt@gmail.com>
2023-12-16 19:08:03 -0800

    Added screenshotsCollectionView

---
+++ GameWorld/ViewControllers/GameDetailsViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GameDetailsViewCellCollectionViewCell.swift
+//  GameWorld
+//
+//  Created by Arseniy Oksenoyt on 12/16/23.
+//
+
+import UIKit
+
+class GameDetailsViewCell: UICollectionViewCell {
+    
+}
