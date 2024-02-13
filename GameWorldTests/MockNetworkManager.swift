a2a589a966cd465cba2196f31b9aa6d84f402db9
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-02-12 16:25:22 -0800

    MockNetworkManager done

--- GameWorldTests/MockNetworkManager.swift
+++ GameWorldTests/MockNetworkManager.swift
@@ -10,4 +10,52 @@ import Foundation
 
 class MockNetworkManager: NetworkManager {
     
+    var shouldReturnError = false
+    var mockedGames: [Game]?
+    var mockedGame: [Game]?
+    var mockedPlatforms: [Platform]?
+    var mockedGameDescription: Game?
+    
+    func fetch<Model>(urlRequest: URLRequest, completion: @escaping (Result<GameWorld.Payload<Model>, Error>) -> Void) where Model : Decodable {
+    }
+    
+    func fetchGames(platform: GameWorld.Platform?, completion: @escaping (Result<[GameWorld.Game], Error>) -> Void) {
+        if shouldReturnError {
+            completion(.failure(NetworkError.decodingError))
+        } else if let games = mockedGames, !games.isEmpty {
+            completion(.success(games))
+        } else {
+            completion(.failure(NetworkError.noData))
+        }
+    }
+    
+    func fetchGame(gameName: String, completion: @escaping (Result<[GameWorld.Game], Error>) -> Void) {
+        if shouldReturnError {
+            completion(.failure(NetworkError.decodingError))
+        } else if let game = mockedGame, !game.isEmpty {
+            completion(.success(game))
+        } else {
+            completion(.failure(NetworkError.noData))
+        }
+    }
+    
+    func fetchPlatforms(completion: @escaping (Result<[GameWorld.Platform], Error>) -> Void) {
+        if shouldReturnError {
+            completion(.failure(NetworkError.decodingError))
+        } else if let platforms = mockedPlatforms, !platforms.isEmpty {
+            completion(.success(platforms))
+        } else {
+            completion(.failure(NetworkError.noData))
+        }
+    }
+    
+    func fetchGameDescription(gameID: Int, completion: @escaping (Result<GameWorld.Game, Error>) -> Void) {
+        if shouldReturnError {
+            completion(.failure(NetworkError.decodingError))
+        } else if let gameDescription = mockedGameDescription {
+            completion(.success(gameDescription))
+        } else {
+            completion(.failure(NetworkError.noData))
+        }
+    }
 }
